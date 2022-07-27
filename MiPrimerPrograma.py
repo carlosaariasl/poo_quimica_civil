31077d95de85f2d9cd3466beda6410431cdd3f02
Carlos Arias <43482712+carlosaariasl@users.noreply.github.com>
2022-07-27 13:05:17 -0600

    Add files via upload

---
+++ MiPrimerPrograma.py
@@ -0,0 +1,25 @@
+class Perro:
+    patas = 4
+    pass
+
+firulais = Perro()
+escubydoo = Perro()
+
+class Persona:
+    pies = 2
+    manos = 2
+    ojos = 2
+    inteligencia = 1
+    estudios = ""
+    pass
+
+carlos = Persona()
+bryan = Persona()
+katherine = Persona()
+fatima = Persona()
+
+carlos.estudios = "Docente"
+bryan.estudios = "Estudiante"
+katherine.estudios = "Estudiante"
+fatima.estudios = "Estudiante"
+
